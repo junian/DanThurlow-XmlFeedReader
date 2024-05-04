7bf30966cdd51d06e35a712d549fd64fbee269fa
Junian Triajianto <author@junian.dev>
2024-05-04 16:32:32 +0700

    Add ViewModel

--- src/XmlFeedReader/Forms/MainForm.cs
+++ src/XmlFeedReader/Forms/MainForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ReactiveUI;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -8,15 +9,44 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using XmlFeedReader.Properties;
+using XmlFeedReader.ViewModels;
 
 namespace XmlFeedReader.Forms
 {
-    public partial class MainForm : Form
+    public partial class MainForm : Form, IViewFor<MainViewModel>
     {
         public MainForm()
         {
             InitializeComponent();
-            this.Icon = Resources.Favicon;
+            this.WhenActivated(block =>
+            {
+                ViewModel.SafeAction = (action) =>
+                {
+                    if (this.IsDisposed)
+                        return;
+
+                    if (this.InvokeRequired)
+                    {
+                        this.Invoke(action);
+                    }
+                    else
+                    {
+                        action?.Invoke();
+                    }
+                };
+
+                this.OneWayBind(ViewModel, vm => vm.AppTitle, view => view.Text);
+                this.OneWayBind(ViewModel, vm => vm.AppIcon, view => view.Icon);
+
+            });
+
+            ViewModel = new MainViewModel();
+        }
+        public MainViewModel ViewModel { get; set; }
+        object IViewFor.ViewModel
+        {
+            get => ViewModel;
+            set => ViewModel = (MainViewModel)ViewModel;
         }
     }
 }
---
+++ src/XmlFeedReader/ViewModels/MainViewModel.cs
@@ -0,0 +1,46 @@
+﻿using Juniansoft.MvvmReady;
+using ReactiveUI;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XmlFeedReader.Properties;
+using XmlFeedReader.Services;
+
+namespace XmlFeedReader.ViewModels
+{
+    public class MainViewModel: ViewModelBase
+    {
+        private readonly AssemblyService _assembly;
+        private readonly ILogger _log;
+
+        public Action<Action> SafeAction { get; set; }
+
+        public MainViewModel()
+        {
+            _assembly = ServiceLocator.Current.Get<AssemblyService>();
+
+            _log = ServiceLocator.Current.Get<ILogger>();
+
+            AppTitle = $"{_assembly.AssemblyProduct} - v{_assembly.AssemblyVersion}";
+            AppIcon = Resources.Favicon;
+        }
+
+        private string _appTitle;
+        public string AppTitle
+        {
+            get => _appTitle;
+            set => this.RaiseAndSetIfChanged(ref _appTitle, value);
+        }
+
+        private Icon _appIcon;
+        public Icon AppIcon
+        {
+            get => _appIcon;
+            set => this.RaiseAndSetIfChanged(ref _appIcon, value);
+        }
+    }
+}
